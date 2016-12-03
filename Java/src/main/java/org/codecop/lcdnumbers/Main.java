b0f742c89f003b0116049998c42b25b2f6578960
Peter Kofler <codecop@users.noreply.github.com>
2016-12-03 22:06:45 +0100

    Fake client code.

---
+++ Java/src/main/java/org/codecop/lcdnumbers/Main.java
@@ -0,0 +1,18 @@
+package org.codecop.lcdnumbers;
+
+/**
+ * Fake client code.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        final LcdDisplay lcdDisplay = new LcdDisplay( //
+                new DigitsSplitter(new NumeralSystem(), new DigitFactory(new Patterns())), //
+                new DigitScaler(new ScalingRepeater()), new DigitPrinter());
+
+        int number = Integer.parseInt(args[0]);
+        Scaling scaling = Scaling.of(Integer.parseInt(args[1]));
+
+        System.out.println(lcdDisplay.toLcd(number, scaling));
+    }
+}
