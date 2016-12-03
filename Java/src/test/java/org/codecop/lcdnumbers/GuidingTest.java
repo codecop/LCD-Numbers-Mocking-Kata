69677ed03517385c49351b2e91f32f8826e537e6
Peter Kofler <codecop@users.noreply.github.com>
2016-12-03 20:26:40 +0100

    Add all patterns.

--- Java/src/test/java/org/codecop/lcdnumbers/GuidingTest.java
+++ Java/src/test/java/org/codecop/lcdnumbers/GuidingTest.java
@@ -4,6 +4,14 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
+
 import org.codecop.lcdnumbers.DigitFactory;
 import org.codecop.lcdnumbers.DigitPrinter;
 import org.codecop.lcdnumbers.DigitScaler;
@@ -53,4 +61,20 @@ public class GuidingTest {
                      "    ||        |\n" + //
                      "      ---  --- \n", lcd);
     }
+
+    @Test
+    public void shouldPrintAllNumbersLcdSizeThree() throws IOException, URISyntaxException {
+        String lcd = lcdDisplay.toLcd(1234567890, 3);
+        String expected = read("/numbers grid 5x9 - size 3.txt");
+        assertEquals(expected, lcd);
+    }
+
+    private String read(String name) throws IOException, URISyntaxException {
+        URL classpathResource = getClass().getResource(name);
+        Path fileSystemResource = Paths.get(classpathResource.toURI());
+        return Files.readAllLines(fileSystemResource). //
+                stream(). //
+                collect(Collectors.joining("\n")) + "\n";
+    }
+    
 }
