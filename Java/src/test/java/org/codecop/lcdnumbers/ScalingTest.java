286dc9e5cbe35e0ef61455a241dfedbc3cbaa29d
Peter Kofler <codecop@users.noreply.github.com>
2017-04-28 00:01:05 +0200

    Replace even values with stubs. Needs Mockito special feature.

--- Java/src/test/java/org/codecop/lcdnumbers/ScalingTest.java
+++ Java/src/test/java/org/codecop/lcdnumbers/ScalingTest.java
@@ -1,17 +1,24 @@
 package org.codecop.lcdnumbers;
 
 import nl.jqno.equalsverifier.EqualsVerifier;
+import org.junit.Rule;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 public class ScalingTest {
 
-    final Runnable block = mock(Runnable.class);
+    @Rule
+    public MockitoRule mockito = MockitoJUnit.rule();
+
+    @Mock
+    Runnable block; // mock
 
     @Test
     public void equalsContract() {
