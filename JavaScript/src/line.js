5448fae20b7f4b3b75b5600e592730bafbfdd522
Peter Kofler <codecop@users.noreply.github.com>
2017-08-21 21:58:16 +0200

    Converted Line.

---
+++ JavaScript/src/line.js
@@ -0,0 +1,22 @@
+exports = typeof window !== "undefined" && window !== null ? window : global;
+
+/**
+ * Value Object of an LCD digit line.
+ */
+function Line(line) {
+    // TODO Objects.requireNonNull(line);
+
+    this.scaleHorizontal = function(repeat, scaling) {
+        // TODO Objects.requireNonNull(repeater);
+        // TODO Objects.requireNonNull(scaling);
+
+        return new Line(line.charAt(0) + repeat(line.charAt(1), scaling) + line.charAt(2));
+    };
+
+    this.toString = function() {
+        return line;
+    };
+
+};
+
+exports.Line = Line;
---
+++ JavaScript/test/line_test.js
@@ -0,0 +1,36 @@
+/* globals describe, it, beforeEach, Line */
+'use strict';
+
+var chai = require('chai');
+var should = chai.should();
+var sinon = require('sinon');
+
+require('../src/line');
+
+var left = 'a';
+var middle = 'b';
+var right = 'c';
+var replaced = 'bb';
+
+describe('Line', function () {
+
+    var scaling; // fake object
+    var repeat; // stub function
+
+    beforeEach(function() {
+        scaling = {};
+    });
+
+    /**
+     * @see "scaling_repeat_test#should repeat character" for contract of stub.
+     */
+    it('should scale middle character horizontally', function () {
+        repeat = sinon.stub().withArgs(middle, scaling).returns(replaced);
+
+        var line = new Line(left + middle + right);
+        var scaledLine = line.scaleHorizontal(repeat, scaling);
+
+        scaledLine.toString().should.equal(left + replaced + right);
+    });
+
+});
