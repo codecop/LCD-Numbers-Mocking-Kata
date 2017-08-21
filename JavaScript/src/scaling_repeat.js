368796a34b3812f2489a4042fbe895f84e2dfc56
Peter Kofler <codecop@users.noreply.github.com>
2017-08-21 21:36:13 +0200

    ported ScalingRepeater.

---
+++ JavaScript/src/scaling_repeat.js
@@ -0,0 +1,21 @@
+exports = typeof window !== "undefined" && window !== null ? window : global;
+
+/**
+ * Repeat objects and characters according to scaling.
+ */
+exports.scalingRepeat = function(element, scaling) {
+    // TODO Objects.requireNonNull(element);
+    // TODO Objects.requireNonNull(scaling);
+
+    var elements = [];
+    scaling.times(function() {
+        elements.push(element)
+    });
+
+    if (typeof element === 'string' || element instanceof String) {
+        return elements.join('');
+    } else {
+        return elements;
+    }
+
+};
