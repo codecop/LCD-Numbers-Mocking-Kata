be25b357c70e3ceaeb29f18072508ca96ab38ea0
Peter Kofler <peter.kofler@code-cop.org>
2017-08-21 21:36:13 +0200

    ported ScalingRepeater.

---
+++ JavaScript/test/scaling_repeat_test.js
@@ -0,0 +1,24 @@
+/* globals describe, it, expect, beforeEach, afterEach, Scaling */
+'use strict';
+
+var chai = require('chai');
+var should = chai.should();
+
+require('../src/scaling_repeat');
+require('../src/scaling');
+
+describe('scalingRepeat', function () {
+
+    it('should repeat character', function () {
+        var repeated = scalingRepeat('c', Scaling.of(3));
+
+        repeated.should.equal('ccc');
+    });
+
+    it('should repeat elements', function () {
+        var repeated = scalingRepeat({id:1}, Scaling.of(2));
+
+        repeated.should.deep.equal([{id:1}, {id:1}]);
+    });
+
+});
