d221b328a0cd2bcae6bd6eba779d3a6164cf434e
Peter Kofler <peter.kofler@code-cop.org>
2017-08-24 23:17:25 +0200

    Rename function.

--- JavaScript/test/render_digits_test.js
+++ JavaScript/test/render_digits_test.js
@@ -1,25 +1,25 @@
-/* globals describe, beforeEach, it, zipWithCombine, createPrintDigit, Line */
+/* globals describe, beforeEach, it, zipWithCombine, createRenderDigits, Line */
 'use strict';
 
 var chai = require('chai');
 var should = chai.should();
 
 require('../src/line');
-require('../src/print_digit');
 require('../src/zip_with_combine');
+require('../src/render_digits');
 
 function digitFrom(line1, line2) {
     var lines = [ new Line(line1), new Line(line2) ];
     return { lines: function() { return lines; }}; // stub Digit
 }
 
-describe('printDigit', function () {
+describe('renderDigits', function () {
 
     var printDigit;
 
     beforeEach(function() {
         // Zip feels technical, so it is rather a helper than a real collaborator.
-        printDigit = createPrintDigit(zipWithCombine);
+        printDigit = createRenderDigits(zipWithCombine);
     });
 
     it('should render digits side by side', function () {
