7935d82afeb9f62d7a04c80188de750cabae89fb
Peter Kofler <codecop@users.noreply.github.com>
2017-08-24 23:37:46 +0200

    Port Patterns.

---
+++ JavaScript/src/patterns_of.js
@@ -0,0 +1,75 @@
+exports = typeof window !== 'undefined' && window !== null ? window : global;
+
+// Java class Patterns
+
+/**
+ * Knows the initial pattern of each LCD digit.
+ */
+var lcdByDigit = [];
+
+function put(digit, lines) {
+    if (lcdByDigit[digit] !== undefined) {
+        throw new RangeError('duplicate pattern definition for digit ' + digit);
+    }
+
+    lcdByDigit[digit] = lines;
+}
+
+put(0, [new Line(' - '), //
+        new Line('| |'), //
+        new Line('   '), //
+        new Line('| |'), //
+        new Line(' - ')]);
+put(1, [new Line('   '), //
+        new Line('  |'), //
+        new Line('   '), //
+        new Line('  |'), //
+        new Line('   ')]);
+put(2, [new Line(' - '), //
+        new Line('  |'), //
+        new Line(' - '), //
+        new Line('|  '), //
+        new Line(' - ')]);
+put(3, [new Line(' - '), //
+        new Line('  |'), //
+        new Line(' - '), //
+        new Line('  |'), //
+        new Line(' - ')]);
+put(4, [new Line('   '), //
+        new Line('| |'), //
+        new Line(' - '), //
+        new Line('  |'), //
+        new Line('   ')]);
+put(5, [new Line(' - '), //
+        new Line('|  '), //
+        new Line(' - '), //
+        new Line('  |'), //
+        new Line(' - ')]);
+put(6, [new Line(' - '), //
+        new Line('|  '), //
+        new Line(' - '), //
+        new Line('| |'), //
+        new Line(' - ')]);
+put(7, [new Line(' - '), //
+        new Line('  |'), //
+        new Line('   '), //
+        new Line('  |'), //
+        new Line('   ')]);
+put(8, [new Line(' - '), //
+        new Line('| |'), //
+        new Line(' - '), //
+        new Line('| |'), //
+        new Line(' - ')]);
+put(9, [new Line(' - '), //
+        new Line('| |'), //
+        new Line(' - '), //
+        new Line('  |'), //
+        new Line(' - ')]);
+
+exports.patternsOf = function(digit) {
+    if (lcdByDigit[digit] === undefined) {
+        throw new RangeError('no pattern defined for digit ' + digit);
+    }
+
+    return lcdByDigit[digit];
+};
---
+++ JavaScript/test/patterns_of_test.js
@@ -0,0 +1,24 @@
+/* globals describe, it, patternsOf, Line */
+'use strict';
+
+var chai = require('chai');
+var should = chai.should();
+var expect = chai.expect;
+
+require('../src/patterns_of');
+require('../src/Line');
+
+describe('patternsOf', function () {
+
+    it('should return pattern 1', function () {
+        patternsOf(1).map(function(line) { return line.toString(); }).
+            should.deep.equal(['   ', '  |', '   ', '  |', '   ']);
+    });
+
+    it('should fail on unknown digit', function () {
+        expect(function() {
+            patternsOf(42);
+        }).to.throw(RangeError);
+    });
+
+});
