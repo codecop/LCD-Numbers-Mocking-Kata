15c43c027e9ef21e46d1be99aad07d6dad1ca492
Peter Kofler <codecop@users.noreply.github.com>
2017-08-27 11:42:26 +0200

    Dummy client to play with LCD.

---
+++ JavaScript/src/index.js
@@ -0,0 +1,38 @@
+'use strict';
+
+// Java class Main
+
+require('./to_lcd');
+require('./convert_number');
+require('./digits_of');
+require('./digit');
+require('./create_digit');
+require('./line');
+require('./patterns_of');
+require('./scale_digits');
+require('./scaling_repeat');
+require('./render_digits');
+require('./zip_with_combine');
+require('./scaling');
+
+/**
+ * Fake client code. Run this script to see LCD Numbers working:
+ * <code>node index.js 12345 2</code>.
+ */
+var toLcd = createToLcd( //
+            // new DigitsSplitter(new NumeralSystem(), new DigitFactory(new Patterns())), //
+            createConvertNumber(createDigitsOf(), createCreateDigit(patternsOf)), //
+            // new DigitScaler(new ScalingRepeater()), //
+            createScaleDigits(scalingRepeat),
+            // new DigitPrinter(new Zipper()));
+            createRenderDigits(zipWithCombine)
+        );
+
+var args = process.argv;
+args.shift(); // node
+args.shift(); // script name
+
+var number = Number(args[0]);
+var scaling = Scaling.of(Number(args[1]));
+
+console.log(toLcd(number, scaling));
