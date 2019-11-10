2b36c426b3284ce61c723fd4754d1642dff7f873
Peter Kofler <peter.kofler@code-cop.org>
2019-11-10 22:13:07 +0100

    Rename test project to .Tests consistently.

--- Csharp/LcdNumbers.Tests/Properties/AssemblyInfo.cs
+++ Csharp/LcdNumbers.Tests/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("LcdNumberTests")]
+[assembly: AssemblyProduct("LcdNumbers.Tests")]
 [assembly: AssemblyTrademark("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
